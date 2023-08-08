0d4c818cc1b8343b1e419eec0b715f74dbfe99bf
inagib21 <inagib21@gmail.com>
2023-08-07 22:59:36 -0400

    final project for now

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -7,6 +7,4 @@ export const client = createClient({
   dataset,
   projectId,
   useCdn,
-  token:
-  "skY3WtgUJNKueJYsyE65QnvimwN4MrnBTr2mmk7387n8jKoyPKZAjcXGswYQLHBMkwd7yi47L6AbCE8in0d3Xj6GKU9Vy0047syD8bbZ12Y4g1eX6HpDVXiAr4I1mQSQHuaJh0EbjV64RojUafw1pnIT2vFme9ApTSg7yK3EMkmo6S2ya72c",
-})
+  })
